a462697b0af49e8430882ed35e9579628fad5879
Inner Drive Studios <info@innerdrivestudios.com>
2022-12-27 12:30:51 +0100

    Finished a grid based optimized version of the snake game that doesn't use colliders

---
+++ Assets/SampleSetup_2_Optimized/Scripts/Snake.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SampleSetup_2_Optimized
+{
+    /**
+     * An optimized snake version where we are no longer using colliders, boundaries, rigidbodies etc
+     * but use a 2d array as a lookup table instead.
+     */
+    public class Snake : MonoBehaviour
+    {
+        //used for initial direction of the snake and possible as an indicator (if it is not disabled)
+        public Transform NextPositionIndicator;
+
+        //reference to the snakefield so that we know where we are and where everything else is
+        public SnakeField SnakeField;
+
+        //prefab to spawn every now and then which will make the snake grow when eaten
+        public Transform ApplePrefab;
+        //spawn rate in seconds
+        public float AppleSpawnDelay;
+        //cached wait for second for the apple spawn
+        private WaitForSeconds AppleSpawnDelayWFS;
+
+        //prefab to spawn at the end of the snake to make it grow when it eats an apple
+        public Transform SnakePartPrefab;
+        //pause between moves 
+        public float SnakeUpdateDelay;
+        //cached wait for second for the snake
+        private WaitForSeconds SnakeUpdateDelayWFS;
+
+        private Vector3 currentDirection;
+        private Vector3 newDirection;
+
+        private bool gameOver = false;
+
+        //Keep track of the body parts to update.
+        //We could also keep track of the children of this object but then we'd be doing unnecessary transform updates.
+        private List<Transform> snakeParts = new List<Transform>();
+
+        void Awake()
+        {
+            //get the initial direction
+            currentDirection = NextPositionIndicator.transform.position - transform.position;
+            currentDirection.Normalize();
+            newDirection = currentDirection;
+
+            //make sure we are on whole positions to start with
+            transform.SnapToGrid(1);
+
+            //make sure the head is also a snake part to update
+            snakeParts.Add(transform);
+
+            SnakeUpdateDelayWFS = new WaitForSeconds(SnakeUpdateDelay);
+            StartCoroutine(snakeUpdate());
+
+            AppleSpawnDelayWFS = new WaitForSeconds(AppleSpawnDelay);
+            StartCoroutine(spawnApples());
+
+        }
+
+        private IEnumerator snakeUpdate()
+        {
+            while (true)
+            {
+                yield return SnakeUpdateDelayWFS;
+
+                //could we move or did we crash into a boundary or ourselves?
+                if (!moveSnake())
+                {
+                    Debug.Log("Game over");
+                    gameOver = true;
+                    StopAllCoroutines();
+                    yield break;
+                }
+            }
+        }
+
+        private IEnumerator spawnApples()
+        {
+            while (!gameOver)
+            {
+                yield return AppleSpawnDelayWFS;
+                spawnApple();
+            }
+        }
+
+        private bool moveSnake()
+        {
+            Vector3 newHeadPosition = transform.position + newDirection;
+
+            //went outside?
+            if (!SnakeField.IsInside(newHeadPosition)) return false;
+
+            //bit own tail?
+            Transform contents = SnakeField.GetContents(newHeadPosition);
+            if (contents != null && contents.CompareTag("SnakePart"))
+            {
+                return false;
+            }
+
+            //store the tail position in case we need to grow, before we move the head
+            Transform tail = snakeParts.Last();
+            Vector3 lastTailPosition = tail.position;
+            SnakeField.Clear(tail);
+
+            //move all elements of the snake from back to front to the position of the element in front of it
+            //except the head (note the i > 0) since it has no predecessor
+            for (int i = snakeParts.Count - 1; i > 0; i--)
+            {
+                snakeParts[i].position = snakeParts[i - 1].position;
+                SnakeField.Store(snakeParts[i]);
+            }
+            
+            //now update head:
+            transform.position = newHeadPosition;
+            SnakeField.Store(transform);
+
+            //and the rest of the administration
+            NextPositionIndicator.position = newHeadPosition + newDirection;
+            currentDirection = newDirection;
+
+            //if we ate something else then our own tail
+            if (contents != null)
+            {
+                SnakeField.Clear(contents);
+                Destroy(contents.gameObject);
+
+                //spawn new tail
+                Transform newTail = Instantiate(SnakePartPrefab, lastTailPosition, Quaternion.identity);
+                snakeParts.Add(newTail);
+                SnakeField.Store(newTail);
+            }
+            
+            return true;
+        }
+
+        private void spawnApple()
+        {
+            Vector3 randomWorldPosition = SnakeField.GetRandomWorldPosition();
+            if (SnakeField.GetContents(randomWorldPosition) == null)
+            {
+                Transform pickup = Instantiate(ApplePrefab, randomWorldPosition, Quaternion.identity);
+                SnakeField.Store(pickup);
+            }
+        }
+
+        private void Update()
+        {
+            if (gameOver) return;
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                newDirection = Vector3.up;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                newDirection = Vector3.down;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                newDirection = Vector3.left;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                newDirection = Vector3.right;
+            }
+
+            if (Vector3.Dot(currentDirection, newDirection) > -0.5f)
+            {
+                NextPositionIndicator.position = transform.position + newDirection;
+            }
+            else
+            {
+                newDirection = currentDirection;
+            }
+        }
+
+    }
+}
---
+++ Assets/SampleSetup_2_Optimized/Scripts/SnakeField.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+namespace SampleSetup_2_Optimized
+{
+    /**
+     * Simple grid that allows us to quickly track where certain elements are.
+     * Do not scale the grid since its scale will be completely ignored.
+     * 
+     * Note that we always assume a grid size of 1 (room for improvement there).
+     */
+    public class SnakeField : MonoBehaviour
+    {
+        public int width;
+        public int height;
+
+        //the offset we need to make sure the center of the grid is at our transform's position
+        //and the offset that we need to convert between gridspace and worldspace
+        private Vector3 _offset = new Vector2();
+
+        //x and y align with worldspace
+        private Transform[,] _grid;
+
+        private void Awake()
+        {
+            OnValidate();
+        }
+
+        private void OnValidate()
+        {
+            if (_grid == null || _grid.GetLength(0) != width || _grid.GetLength(1) != height)
+            {
+                //if the grid size changes reset the grid's backing store and the offset we use for conversion
+                //Required offset based on grid size to keep grid centered on our transform:
+                //1 -> 0, 2 -> -.5, 3 -> -1, 4 -> -1.5, etc
+                _offset = new Vector3((-width / 2.0f) + 0.5f, (-height / 2.0f) + 0.5f, 0);
+                _grid = new Transform[width, height];
+            }
+        }
+
+        private Vector2Int getGridIndex(Vector3 pWorldPosition)
+        {
+            //we take the given world position, first make it relative to our own position, then relative to the 
+            //topleft position of the grid. Round what is left, we always assume a grid size of one.
+            Vector3 localPosition = pWorldPosition - transform.position - _offset;
+            localPosition.x = Mathf.Round(localPosition.x);
+            localPosition.y = Mathf.Round(localPosition.y);
+            localPosition.z = Mathf.Round(localPosition.z);
+
+            return new Vector2Int((int)localPosition.x, (int)localPosition.y);
+        }
+
+        public bool IsInside(Vector3 pWorldPosition)
+        {
+            Vector2Int localPosition = getGridIndex(pWorldPosition);
+            return (localPosition.x >= 0 && localPosition.x < width && localPosition.y >= 0 && localPosition.y < height);
+        }
+
+        public Transform GetContents(Vector3 pWorldPosition)
+        {
+            Vector2Int localPosition = getGridIndex(pWorldPosition);
+
+            if (localPosition.x >= 0 && localPosition.x < width && localPosition.y >= 0 && localPosition.y < height)
+            {
+                return _grid[localPosition.x, localPosition.y];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Clear(Transform pTransform)
+        {
+            Vector2Int gridIndex = getGridIndex(pTransform.position);
+            _grid[gridIndex.x, gridIndex.y] = null;
+        }
+
+        public void Store(Transform pTransform)
+        {
+            Vector2Int gridIndex = getGridIndex(pTransform.position);
+            _grid[gridIndex.x, gridIndex.y] = pTransform;
+        }
+
+        public Vector3 GetRandomWorldPosition()
+        {
+            Vector3 localPosition = new Vector3(Random.Range(0, width), Random.Range(0, height), 0);
+            Vector3 worldPosition = localPosition + transform.position + _offset;
+            return worldPosition;
+        }
+
+        private void OnDrawGizmos()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (_grid[x, y] == null) ? new Color(0, 1, 0, 0.3f) : new Color(1, 0, 0, 0.3f);
+                    Vector3 worldPosition = transform.position + new Vector3(x, y, 0) + new Vector3(_offset.x, _offset.y, 0);
+                    Gizmos.DrawCube(worldPosition, new Vector3(0.9f, 0.9f, 0.1f));
+                }
+            }
+        }
+
+    }
+}
